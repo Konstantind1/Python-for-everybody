c821cbcdbe7d20442017d376097850ca941d8212
Konstantind1 <31651378+Konstantind1@users.noreply.github.com>
2018-02-04 23:56:37 +0300

    Add files via upload

---
+++ hw7/homework7.py
@@ -0,0 +1,54 @@
+def getfile(words compilations):
+    with open (words compilation,encoding ="utf-8") as f:
+        mots=text.split()
+        syntax=.!?;:""-
+        text= f.read()
+        for count, mots in enumerate(mots):
+            mots[count]=word.strip(syntax)
+        return mots
+def main()
+    dict={}
+    f=input('saisir le nom" ')
+    if f == " "
+       print)("desole")
+    else:
+        mots=words compilation(f)
+        print(cherch(mots,dict))
+        omni = input("puis-je saisir un mot avec la omni prefixe: ")
+        if omni =" ":
+            print('Probablement, vous s "etes trompes un petit peu")
+        else:
+            print(cherch numero 2(omni,dict))
+        print(
+                
+def cherch (omni, vocab):
+    if omni in dict:
+        parole = dict[omni]
+    else:
+        dict= "Malheureusement, ce mot n'existe pas:("
+    return parole
+
+def cherch numero 2(mots,dict):
+    count = 0
+    for mot in dict:
+        if mot[4:] == "omni":
+            if mot in dict:
+                dict[mot] +=1
+            else:
+                dict[mot] =1
+            
+    return dict
+
+            
+
+    
+    
+
+            
+
+
+    
+        
+        
+       
+        
