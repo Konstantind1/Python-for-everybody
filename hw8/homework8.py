7b97bd867957296c86da8729bfb7c281a9516243
Konstantind1 <31651378+Konstantind1@users.noreply.github.com>
2018-02-18 23:48:25 +0300

    Add files via upload

---
+++ hw8/homework8.py
@@ -0,0 +1,51 @@
+"Я не успел дописать дополнительные ограничения, не судите строго(("
+import random
+dict = {}
+
+
+def openfile(slova.txt):
+    with open(slova.txt,encoding='utf-8' ) as f:
+        sequence = f.read()
+        slova = text.split('\n')
+    return slova
+
+def task(dict,n):
+
+
+    
+def slovari(text,dict):
+  
+    count = len(sequence)
+    slovlist = []
+    for i in range(1,len(sequence):
+        a = sequence[i]+ 
+            
+        
+    
+
+
+def main():
+    file = slovari(openfile('slovechki.txt'),d)
+    if random.randint(1,6) == 6:
+        a = task(dict, 'здравствуйте')
+        print(a)
+    elif random.randint(1,6) == 1:
+        a = task(dict, 'я')
+        print(a)
+    elif random.randint(1,6) = 5 :
+        a = task(dict, 'ваша')
+        print(a)
+    elif random.randint(1,6) = 2 :
+        a = task(dict, 'тётя')
+        print(a)
+    elif random.randint(1,6) = 4 :
+        a = task(dict, 'как')
+        print(a)
+    elif random.randint(1,6) = 3:
+        a = task(dictm 'оно')
+        print(a)
+                
+    
+
+if_imya_== '__main__':
+    main()
