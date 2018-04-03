11328ad80da608c78d47cc9fa12989b1ab5689cf
Konstantind1 <31651378+Konstantind1@users.noreply.github.com>
2018-04-03 22:51:50 +0300

    Add files via upload

---
+++ hw-additional/hw-additional.py
@@ -0,0 +1,59 @@
+"Дополнительное дз(penalty)"
+from re import findall
+
+def main():
+    start = input('начальная директория' :)
+    finish = input('конечнная директория' :)
+
+    
+def openfile(file):
+    with open(name, 'r', encoding='utf-8') as f:
+        return f.read()
+
+
+def resultfile(file):
+    with open(name, 'w', encoding='utf-8') as f:
+        f.write(str(text))
+
+
+def first_check():
+    f.write(str(re.findall("[a-z]@gmail.com", text)))
+    f.write(str(re.findall("[a-z][a-z]@gmail.com", text)))
+    f.write(str(re.findall("[a-z][a-z][a-z]@gmail.com", text)))
+    f.write(str(re.findall("[a-z][a-z][a-z][a-z]@gmail.com", text)))
+    f.write(str(re.findall("[a-z][a-z][a-z][a-z][a-z]@gmail.com", text)))
+    f.write(str(re.findall("[a-z][a-z][a-z][a-z][a-z][a-z]@gmail.com", text)))
+    f.write(str(re.findall("[a-z][a-z][a-z][a-z][a-z][a-z][a-z]@gmail.com", text)))
+    f.write(str(re.findall("[a-z][a-z][a-z][a-z][a-z][a-z][a-z][a-z]@gmail.com", text)))
+    f.write(str(re.findall("[a-z][a-z][a-z][a-z][a-z][a-z][a-z][a-z][a-z]@gmail.com", text)))
+   
+    
+
+    
+
+    
+    
+
+
+
+    
+
+        
+
+    
+    
+
+
+
+    
+
+        
+
+    
+    
+
+
+
+    
+
+        
